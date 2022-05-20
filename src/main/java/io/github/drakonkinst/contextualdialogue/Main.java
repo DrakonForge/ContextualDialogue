5165976799955443903b565c316e2433778b3713
Wesley Ho <weho@chapman.edu>
2022-05-20 09:12:57 -0700

    Add statistics

--- src/main/java/io/github/drakonkinst/contextualdialogue/Main.java
+++ src/main/java/io/github/drakonkinst/contextualdialogue/Main.java
@@ -1,7 +1,7 @@
 package io.github.drakonkinst.contextualdialogue;
 
-import io.github.drakonkinst.contextualdialogue.commonutil.JsonUtils;
-import io.github.drakonkinst.contextualdialogue.commonutil.MyLogger;
+import io.github.drakonkinst.commonutil.JsonUtils;
+import io.github.drakonkinst.commonutil.MyLogger;
 import io.github.drakonkinst.contextualdialogue.context.ContextTable;
 import io.github.drakonkinst.contextualdialogue.json.ContextParser;
 import io.github.drakonkinst.contextualdialogue.speech.SpeechQuery;
@@ -10,8 +10,10 @@ import io.github.drakonkinst.contextualdialogue.speech.SpeechbankDatabase;
 import io.github.drakonkinst.contextualdialogue.speech.text.TextToken;
 
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 
 public class Main {
@@ -30,6 +32,8 @@ public class Main {
         boolean verbose = Boolean.parseBoolean(args[5]);
         boolean printLines = Boolean.parseBoolean(args[6]);
         boolean tokenize = Boolean.parseBoolean(args[7]);
+        boolean showPriority = Boolean.parseBoolean(args[8]);
+        boolean showStats = Boolean.parseBoolean(args[9]);
 
         if(verbose) {
             MyLogger.initialize(Level.FINEST);
@@ -57,21 +61,42 @@ public class Main {
         MyLogger.info();
         SpeechbankDatabase database = SpeechbankDatabase.getInstance();
         start = System.currentTimeMillis();
+        Set<String> generated = new HashSet<>();
+        int numRepeats = 0;
         for(int i = 0; i < howMany; ++i) {
             SpeechQuery query = new SpeechQuery(contexts, SymbolChecker.functionLookup);
             SpeechResult result = database.generateLine(group, category, query);
 
             if(printLines) {
-                if(tokenize) {
-                    MyLogger.info(tokensToString(result.getTextTokens()));
+                if(result == null) {
+                    MyLogger.info("<failed to generate>");
                 } else {
-                    MyLogger.info(result.getText());
+                    String message = "";
+                    String text = result.getText();
+                    if(showPriority) {
+                        message = "(" + result.getPriority() + ") ";
+                    }
+                    if(tokenize) {
+                        message += tokensToString(result.getTextTokens());
+                    } else {
+                        message += text;
+                    }
+                    MyLogger.info(message);
+                    if(!generated.add(text)) {
+                        ++numRepeats;
+                    }
                 }
             }
         }
         end = System.currentTimeMillis();
         MyLogger.info();
         MyLogger.info("Took " + (end - start) + "ms to generate " + howMany + " speech lines");
+
+        if(showStats) {
+            MyLogger.info("Ran into " + numRepeats + " repeats");
+            MyLogger.info("This query checks a maximum of " + Statistics.countNumCheckedRules(group, category, contexts) + " rules");
+            MyLogger.info("And has approximately " + Statistics.countNumVariations(group, category, contexts) + " possible variations");
+        }
     }
 
     private static String tokensToString(List<TextToken> tokens) {
---
+++ src/main/java/io/github/drakonkinst/contextualdialogue/Statistics.java
@@ -0,0 +1,89 @@
+package io.github.drakonkinst.contextualdialogue;
+
+import io.github.drakonkinst.contextualdialogue.context.ContextTable;
+import io.github.drakonkinst.contextualdialogue.rule.CriterionFail;
+import io.github.drakonkinst.contextualdialogue.rule.Rule;
+import io.github.drakonkinst.contextualdialogue.speech.Speechbank;
+import io.github.drakonkinst.contextualdialogue.speech.SpeechbankDatabase;
+import io.github.drakonkinst.contextualdialogue.speech.SpeechbankEntry;
+import io.github.drakonkinst.contextualdialogue.token.Token;
+import io.github.drakonkinst.contextualdialogue.token.TokenGroup;
+import io.github.drakonkinst.contextualdialogue.token.TokenList;
+
+import java.util.List;
+import java.util.Map;
+
+public final class Statistics {
+    public static long countNumVariations(Token token) {
+        long variations = 1;
+        if(token instanceof TokenGroup tokenGroup) {
+            List<Token> tokens = tokenGroup.getTokens();
+            for(Token t : tokens) {
+                variations *= countNumVariations(t);
+            }
+        } else if(token instanceof TokenList tokenList) {
+            List<Token> tokens = tokenList.getTokens();
+            variations = tokens.size();
+        }
+        return variations;
+    }
+
+    public static long countNumVariations(SpeechbankEntry entry) {
+        TokenGroup[] tokens = entry.getSpeechLines();
+        long variations = 0;
+        for(TokenGroup t : tokens) {
+            variations += countNumVariations(t);
+        }
+        return variations;
+    }
+
+    public static long countNumVariations(String groupName, String category, Map<String, ContextTable> contexts) {
+        long variations = 0;
+        Speechbank speechbank = SpeechbankDatabase.getInstance().getSpeechbank(groupName);
+        if(speechbank == null) {
+            return 0;
+        }
+        SpeechbankEntry[] entries = speechbank.getEntriesFor(category);
+        for(SpeechbankEntry entry : entries) {
+            boolean couldMatch = Speechbank.match(entry.getRule(), contexts, true);
+            if(couldMatch) {
+                variations += countNumVariations(entry);
+            }
+        }
+        return variations;
+    }
+
+    public static long countNumCheckedRules(String groupName, String category, Map<String, ContextTable> contexts) {
+        long rulesChecked = 0;
+        Speechbank speechbank = SpeechbankDatabase.getInstance().getSpeechbank(groupName);
+        if(speechbank == null) {
+            return 0;
+        }
+        SpeechbankEntry[] entries = speechbank.getEntriesFor(category);
+        int highestMatchingPriority = -999;
+        for(SpeechbankEntry entry : entries) {
+            int priority = entry.getRule().getPriority();
+            if(priority < highestMatchingPriority) {
+                break;
+            }
+            ++rulesChecked;
+            boolean couldMatch = Speechbank.match(entry.getRule(), contexts, false);
+            if(couldMatch && !containsFailCriterion(entry.getRule())) {
+                // Definitely matches, this can cause an early stop
+                if(priority > highestMatchingPriority) {
+                    highestMatchingPriority = priority;
+                }
+            }
+        }
+        return rulesChecked;
+    }
+
+    private static boolean containsFailCriterion(Rule rule) {
+        for(int i = 0; i < rule.getSize(); ++i) {
+            if(rule.getTupleAt(i).getCriterion() instanceof CriterionFail) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
